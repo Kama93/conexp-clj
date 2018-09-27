9cd3d42f4ea951ea73824ce3246abe2221c99707
Maximilian Stubbemann <35598183+Kama93@users.noreply.github.com>
2018-09-27 16:13:56 +0200

    Implemented tests for modularity
    Con5inued working on tests for modularity

--- src/test/clojure/conexp/fca/applications/socialanalytics_test.clj
+++ src/test/clojure/conexp/fca/applications/socialanalytics_test.clj
@@ -11,7 +11,8 @@
                                          random-contexts
                                          make-context-from-matrix
                                          objects
-                                         attributes]]
+                                         attributes
+                                         incidence-relation]]
             [conexp.fca.applications.socialanalytics :refer :all]
             [clojure.test :refer [deftest is]]
             [conexp.base :refer [with-testing-data]]))
@@ -152,6 +153,83 @@
                            n (count (attributes ctx))]
                        (and (= (count degrees) n)
                             (every? #(<= 0 % n) degrees)))))
+
+;;;Modularity
+
+(defn- close?
+  ([x y epsilon]
+   (<= (Math/abs (- x y)) epsilon))
+  ([x y] (close? x y 0.0001)))
+
+(deftest test-modularity
+  (let [g {1 #{1 2 3} 2 #{1}
+           3 #{1}}
+        h {1 #{2 3 4} 2 #{1 3 4}
+           3 #{1 2 4 5} 4 #{1 2 3}
+           5 #{3 6 7} 6 #{5 8}
+           7 #{5 9} 8 #{6 9}
+           9 #{7 8}}]
+    (is (close? (modularity g [#{1,2} #{3}])
+                (/ 7 36)))
+    (is (= (modularity h
+                       #{#{1 2 3 4}
+                         #{5 6 7}
+                         #{8 9}})
+           0.34375))))
+
+(deftest test-context-graph-modularity
+  (let [ctx (make-context-from-matrix 4 4
+                                     [1 0 1 1
+                                      0 1 1 0
+                                      1 0 1 0
+                                      0 0 1 1])]
+    (is (close? (context-graph-modularity ctx
+                                          [{:objects #{0 1} :attributes #{0 1}},
+                                           {:objects #{2 3} :attributes #{2 3}}])
+                (/ 4 81))))
+  (with-testing-data [ctx (random-contexts 10 10)]
+                      (or (empty? (incidence-relation ctx))
+                          (= 0.0
+                             (context-graph-modularity
+                               ctx
+                               #{{:objects (objects ctx)
+                                  :attributes (attributes ctx)}})))))
+
+(deftest test-object-projection-modularity
+  (let [ctx (make-context-from-matrix ['a 'b 'c 'd] [0 1 2 3]
+                                      [1 0 0 1
+                                       0 1 1 0
+                                       1 1 0 0
+                                       0 0 0 1])]
+    (is (close? (object-projection-modularity ctx [#{'a 'c} #{'b} #{'d}])
+                (/ 96 196))))
+  (with-testing-data [ctx (random-contexts 20 20)]
+                     (let [n (quot (count (objects ctx))
+                                   2)]
+                       (is (or (empty? (incidence-relation ctx))
+                               (<= -1
+                                   (object-projection-modularity ctx
+                                                                 [(set (take n (objects ctx))),
+                                                                  (set (drop n (objects ctx)))]))
+                               1)))))
+
+(deftest test-attribute-projection-modularity
+  (let [ctx (make-context-from-matrix ['a 'b 'c 'd] [0 1 2 3]
+                                      [1 0 0 1
+                                       0 1 1 0
+                                       1 1 0 0
+                                       0 0 0 1])]
+    (is (close? (attribute-projection-modularity ctx [#{0 1} #{2 3}])
+                (/ 88 196))))
+  (with-testing-data [ctx (random-contexts 20 20)]
+                     (let [n (quot (count (attributes ctx))
+                                   2)]
+                       (is (or (empty? (incidence-relation ctx))
+                               (<= -1
+                                   (attribute-projection-modularity ctx
+                                                                 [(set (take n (attributes ctx))),
+                                                                  (set (drop n (attributes ctx)))]))
+                               1)))))
 
 ;;;
 
